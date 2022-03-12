e7c64c41be34c2c028a0d2bc2f893b6b2879ad05
keylesssky187 <60062880+keylesssky187@users.noreply.github.com>
2022-03-12 16:20:28 +0300

    =

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        pager: false,
+    })
+})
+
+$(document).ready(function(){
+    $('.bxslider_team').bxSlider({
+        controls: false,
+    })
+})
